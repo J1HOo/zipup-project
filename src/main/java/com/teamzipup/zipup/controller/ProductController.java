7215f6e8e16e6885cc6ac2dd2a57fc403c0ce76d
J1HOo <businessj1ho@gmail.com>
2024-12-27 19:33:34 +0900

    컨트롤러 수정

--- src/main/java/com/teamzipup/zipup/controller/ProductController.java
+++ src/main/java/com/teamzipup/zipup/controller/ProductController.java
@@ -16,36 +16,35 @@ public class ProductController {
 
     @PostMapping("/product/add")
     public String productAdd(@RequestParam("id") long id,
-                             MultipartFile image,
                              @RequestParam("productName") String productName,
                              @RequestParam("price") int price,
                              @RequestParam("option1") String option1,
                              @RequestParam("option2") String option2,
                              @RequestParam("option3") String option3,
                              @RequestParam("category") String category,
-                             MultipartFile description,
+                             @RequestParam("description") MultipartFile description,
+                             @RequestParam("image") MultipartFile image,
                              HttpSession session,
                              Model model) {
-        // 세션에서 로그인된 사용자 정보 가져오기
         User loginUser = (User) session.getAttribute("loginUser");
 
         if (loginUser == null) {
             model.addAttribute("error", "로그인이 필요합니다.");
-            return "redirect:/login"; // 로그인 페이지로 이동
+            return "redirect:/login";
         }
 
-        // 역할 확인
         if (!"SELLER".equals(loginUser.getRole())) {
             model.addAttribute("error", "상품 등록 권한이 없습니다.");
-            return "index"; // 권한 없음 페이지로 이동
+            return "redirect:/";
         }
 
-        long sellerId = loginUser.getId(); // 세션에서 가져온 사용자 ID
+        long sellerId = loginUser.getId();
 
-        // 상품 등록 로직 호출
+        // 서비스 호출
         productService.insertProduct(id, sellerId, image, productName, price, option1, option2, option3, category, description);
 
         model.addAttribute("msg", "상품 등록이 완료되었습니다.");
-        return "redirect:/product/Detail/" + id; // 등록된 productDetail.html로 이동
+        return "redirect:/product/Detail/" + id;
     }
+
 }
